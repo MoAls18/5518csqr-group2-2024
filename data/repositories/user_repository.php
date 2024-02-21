2d3300c178daf610003965ea85f65e8a61337298
MoAls18 <malsaadi124@gmail.com>
2024-02-21 15:03:42 +0300

    Added further CRUD methods for users repository

--- data/repositories/user_repository.php
+++ data/repositories/user_repository.php
@@ -28,6 +28,106 @@ class UserRepository
         $stmt->bind_param("sssss", $user->getUsername(), $user->getEmail(), $user->getPassword(), $user->getCreatedAt(), $user->getUpdatedAt());
         return $stmt->execute();
     }
+
+    /**
+     * Gets user by id.
+     *
+     * This function accesses the database to fetch data of a user by their id.
+     *
+     * @param  int $id id of the user
+     * @return User
+     **/
+    public function getUserById(int $id): User
+    {
+
+        $query = "SELECT id, username, email, created_at, updated_at FROM users WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 0) {
+            return null;
+        }
+
+        $user_data = $result->fetch_assoc();
+
+        $user = new User(
+            id: $user_data['id'],
+            username: $user_data['username'],
+            email: $user_data['email'],
+            password: null, // password is set to null as retrieving the password in plaintext is a security risk
+            created_at:$user_data['created_at'],
+            updated_at: $user_data['updated_at']
+        );
+
+        return $user;
+    }
+  
+    /**
+     * Gets User by username
+     *
+     * Retrieves data of User from username
+     *
+     * @param  string $username username of site user
+     * @return User
+     **/
+    public function getUserByUsername(string $username): User
+    {
+        $query = "SELECT id, username, email, created_at, updated_at FROM users WHERE username = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 0) {
+            return null;
+        }
+
+        $user_data = $result->fetch_assoc();
+
+        $user = new User(
+            id: $user_data['id'],
+            username: $user_data['username'],
+            email: $user_data['email'],
+            password: null, // password is set to null as retrieving the password in plaintext is a security risk
+            created_at:$user_data['created_at'],
+            updated_at: $user_data['updated_at']
+        );
+
+        return $user;
+    }
+    
+    /**
+     * Deletes User from Database
+     *
+     * Deletes the user from the database based on their id.
+     *
+     * @param  int $id id of the User
+     * @return void
+     **/
+    public function deleteUserById(int $id): void
+    {
+        $query = "DELETE FROM users WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+    }
+    public function deleteUserByUsername(string $username): void
+    {
+        $query = "DELETE FROM users WHERE username = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("i", $username);
+        $stmt->execute();
+    }
+
+    public function updateUserPassword(User $user, string $password): void
+    {
+        $query = "UPDATE users SET password = ? WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("si", $password, $user->getId());
+        $stmt->execute();
+    }
 }
 
 ?>
