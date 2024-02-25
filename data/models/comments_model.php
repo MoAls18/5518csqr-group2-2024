144918b6f3af45e0992b5943ef3a1570aab8e099
MoAls18 <malsaadi124@gmail.com>
2024-02-25 14:11:07 +0300

    Added documentation and functions for comment model

--- data/models/comments_model.php
+++ data/models/comments_model.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Comment Class
+ * This class holds information for the comments records in the database and is
+ * used to interact with the BLL.
+ * The class doesn't contain any setters for security reasons as all updates are
+ * handled by the repository classes.
+ */
 class Comment
 {
     private $id;
@@ -9,6 +16,9 @@ class Comment
     private $created_at;
     private $updated_at;
   
+    /** 
+     * Comment constructor
+     */
     public function __construct(int $id, string $content, int $user_id, int $post_id, string $created_at, string $updated_at)
     {
         $this->id = $id;
@@ -17,5 +27,65 @@ class Comment
         $this->post_id = $post_id;
         $this->created_at = $created_at;
         $this->updated_at = $updated_at;
+    }
+
+    /**
+     * Get the id of the comment.
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Gets the content of the comment.
+     *
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * Gets the id of the User that posted the comment.
+     *
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Gets the id of the Post that posted the comment.
+     *
+     * @return int
+     */
+    public function getPostId(): int
+    {
+        return $this->post_id;
+    }
+  
+    /**
+     * Gets the time stamp the comment was created at.
+     *
+     * @return int
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Gets the time stamp the comment was updated at.
+     *
+     * @return int
+     */
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
     }
 }
