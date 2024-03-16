8b682d2ca3be35c313116807aec5ac1a44dffbff
MoAls18 <malsaadi124@gmail.com>
2024-03-16 20:24:24 +0300

    removed hashing

--- app/services/user_service.php
+++ app/services/user_service.php
@@ -25,36 +25,39 @@ class UserService
         $this->userValidator = new UserValidator();
     }
     
-    public function addUser($email, $username, $password, $repeat): bool{
-        $userExists = $this->userRepository->userExists($username,$email);
-        if($userExists){
+    public function addUser($email, $username, $password, $repeat): bool
+    {
+        $userExists = $this->userRepository->userExists($username, $email);
+        if($userExists) {
             echo "Error: username or email already exists try another one";
             return false;
         }else{
-        $this->userValidator->verifyUserCreation($email, $username, $password, $repeat);
-        $current_timestamp = date(format: "Y-m-d H:i:s");
-        $hashing = password_hash($password, PASSWORD_DEFAULT);
-        $user = new User(rand(), $username, $email, $hashing, $current_timestamp, $current_timestamp);
-        return $this->userRepository->addUser($user);
-    }
+            $this->userValidator->verifyUserCreation($email, $username, $password, $repeat);
+            $current_timestamp = date(format: "Y-m-d H:i:s");
+            $user = new User(rand(), $username, $email, $password, $current_timestamp, $current_timestamp);
+            return $this->userRepository->addUser($user);
+        }
 
     }
 
-    public function getUserByUsername($username){
+    public function getUserByUsername($username)
+    {
         return $this->userRepository->getUserByUsername($username);
     }
 
-    public function getUserByTokenNumber($token){
+    public function getUserByTokenNumber($token)
+    {
         return $this->userRepository->getUserByTokenNumber($token);
     }
     
-    public function setNewUserTokenAndSendResetEmail($email){
+    public function setNewUserTokenAndSendResetEmail($email)
+    {
         
         $user = $this->userRepository->getUserByEmail($email);
-        if($user){
+        if($user) {
             $token = md5(rand());
             $isUpdated = $this->userRepository->updateUserToken($user, $token);
-            if($isUpdated){
+            if($isUpdated) {
                 $this->sendResetPasswordEmail($user->getEmail(), $token);
             }else{
                 echo "Failed to set new token";
@@ -64,54 +67,57 @@ class UserService
         }
     }
 
-    public function updateUserPassword($username, $password, $confirm_password): bool{
-        if($password === $confirm_password){
+    public function updateUserPassword($username, $password, $confirm_password): bool
+    {
+        if($password === $confirm_password) {
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            return $this->userRepository->updateUserPassword($username,$hashed_password ) ;
+            return $this->userRepository->updateUserPassword($username, $hashed_password);
         }else{
             echo "Error: passwords don't match try again!";
             return 0;
         }
     }
 
-    public function updateProfile($userId, $username, $email) {
+    public function updateProfile($userId, $username, $email)
+    {
         return $this->userRepository->updateProfile($userId, $username, $email);
     }
 
-    public function sendResetPasswordEmail($email,$token) {
-    //Import PHPMailer classes into the global namespace
-    //These must be at the top of your script, not inside a function
+    public function sendResetPasswordEmail($email,$token)
+    {
+        //Import PHPMailer classes into the global namespace
+        //These must be at the top of your script, not inside a function
 
     
-    //Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
     
-    //iaip izur izxm rnjp
-    try {
-        //Server settings
-        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'coursework047@gmail.com';                     //SMTP username
-        $mail->Password   = 'xgwmxhpoxwidwjxb';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        //Recipients
-        $mail->setFrom('k.alemadi.01@gmail.com', 'khalid');
-        $mail->addAddress($email, 'Joe User');     //Add a recipient  //Name is optional
+        //iaip izur izxm rnjp
+        try {
+            //Server settings
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'coursework047@gmail.com';                     //SMTP username
+            $mail->Password   = 'xgwmxhpoxwidwjxb';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            //Recipients
+            $mail->setFrom('k.alemadi.01@gmail.com', 'khalid');
+            $mail->addAddress($email, 'Joe User');     //Add a recipient  //Name is optional
     
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = "you can reset your password by clicking this link: http://localhost/5518csqr-group2-2024/public/reset_password.php?token=$token";
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = "you can reset your password by clicking this link: http://localhost/5518csqr-group2-2024/public/reset_password.php?token=$token";
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
 
     }
 }
