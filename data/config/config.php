29931b14f9d932841c321b4f4cfc3ef42705afbe
MoAls18 <malsaadi124@gmail.com>
2024-03-16 15:38:12 +0300

    updated db config

--- data/config/config.php
+++ data/config/config.php
@@ -2,8 +2,8 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', 'coursework');
-  define('DB_PORT', 3308);
+  define('DB_NAME', 'blog');
+  define('DB_PORT', 3306);
 
   
 
